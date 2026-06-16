44085990545cc39e98f3be706499f1fa561067cb
Harsh Patel <harshpatel15505@gmail.com>
2026-06-16 17:35:57 +0530

    comment added

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -12,3 +12,12 @@ const storage = multer.diskStorage({
  export const upload = multer({ 
      storage ,
      });
+
+
+    //  Multer is Express middleware used to process multipart/form-data and extract uploaded files.
+    //  It can temporarily store files on the server using diskStorage().
+    //  Cloudinary is a cloud media management service used for permanent storage, optimization,
+    //  and delivery of images/videos through URLs. Typically, Multer receives the file from the client 
+    // and stores it temporarily in a local folder like public/temp. Then the backend uploads that file to Cloudinary,
+    //  receives a public URL, stores that URL in MongoDB, and optionally deletes the temporary local file.
+    //  MongoDB generally stores only the URL, not the actual image, which keeps the database lightweight and efficient
